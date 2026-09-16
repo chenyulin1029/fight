18b410eee892d24d51483eccfe58dd26cd97bf9b
chenyulin1029 <chenyulin1029@gmail.com>
2026-09-16 09:23:01 +0800

    feat: define consumer runtime notices

---
+++ filedone/runtime/UserNotice.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace filedone {
+
+void ShowOperationFailedNotice() noexcept;
+void ShowRuntimeUnavailableNotice() noexcept;
+void ShowInvalidRequestNotice() noexcept;
+
+} // namespace filedone
